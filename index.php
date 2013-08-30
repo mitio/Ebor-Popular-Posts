ed96c0dad6c116c07c10034a61bad79281c1304a
Tom Rhodes <hello@madeinebor.com>
2013-08-30 10:41:48 +0100

    Added Seabird

--- index.php
+++ index.php
@@ -41,7 +41,27 @@ class ebor_popular_Widget extends WP_Widget {
 			echo  $before_title.$title.$after_title;
 		} ?>
 		
+			<?php if( wp_get_theme() == 'Seabird' ) : ?>
+				<ul class="post-list">
 				
+				<?php query_posts('post_type=post&posts_per_page=' . $instance['amount'] . '&orderby=comment_count&order=DESC'); while ( have_posts() ): the_post(); ?>
+				
+				  <li>
+				    <figure class="overlay">
+				    	<a href="<?php the_permalink(); ?>">
+				    		<?php the_post_thumbnail('thumbnail'); ?>
+				     		<div></div>
+				        </a>
+				    </figure>
+				    <div class="meta">
+				      <h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+				      <em><?php the_time(get_option('date_format')); ?></em>
+				    </div>
+				  </li>
+				  
+				  <?php endwhile; wp_reset_query(); ?> 
+				</ul>
+			<?php else : ?>
 				<dl>
 				  <?php $popular = new WP_Query('post_type=post&posts_per_page=' . $instance['amount'] . '&orderby=comment_count&order=DESC'); if( $popular->have_posts() ) : while ( $popular->have_posts() ): $popular->the_post(); ?>
 				
@@ -50,6 +70,7 @@ class ebor_popular_Widget extends WP_Widget {
 				  
 				  <?php endwhile; endif; wp_reset_query(); ?> 
 				</dl>
+			<?php endif; ?>
 		
 		
 		<?php echo $after_widget;
