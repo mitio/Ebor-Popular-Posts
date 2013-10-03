b0e63d3b420877af8e72635819bdf27980b989b3
Tom Rhodes <hello@madeinebor.com>
2013-10-03 09:18:03 +0100

    Added Marble

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ class ebor_popular_Widget extends WP_Widget {
 		if($title) {
 			echo  $before_title.$title.$after_title;
 		} ?>
-		
+
 			<?php if( wp_get_theme() == 'Seabird' || wp_get_theme() == 'Kyte' ) : ?>
 				<ul class="post-list">
 				
@@ -70,6 +70,23 @@ class ebor_popular_Widget extends WP_Widget {
 			          </li>
 		            <?php endwhile; wp_reset_query(); ?> 
 		        </ul>
+		    <?php elseif( wp_get_theme() == 'Marble' || wp_get_theme()->parent() == 'Marble' ) : ?>
+		    	<ul class="post-list">
+			    	<?php query_posts('post_type=post&posts_per_page=' . $instance['amount'] . '&orderby=comment_count&order=DESC'); if( have_posts() ) : while ( have_posts() ): the_post(); ?>
+			    	  <li>
+			    	    <div class="overlay cap-icon more">
+				    	    <a href="<?php the_permalink(); ?>">
+					    	    <?php the_post_thumbnail('thumbnail'); ?>
+					    	    <div></div>
+				    	    </a>
+			    	    </div>
+			    	    <div class="meta">
+			    	      <h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+			    	      <em><?php the_time( get_option('date_format') ); ?></em>
+			    	    </div>
+			    	  </li>
+			    	<?php endwhile; endif; wp_reset_query(); ?>
+		    	</ul>
 			<?php else : ?>
 				<dl>
 				  <?php $popular = new WP_Query('post_type=post&posts_per_page=' . $instance['amount'] . '&orderby=comment_count&order=DESC'); if( $popular->have_posts() ) : while ( $popular->have_posts() ): $popular->the_post(); ?>
